3818460b7dc997f04f99c48bddb5c4710d226617
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2018-09-11 11:17:47 +0200

    test failed

--- primeservice/PrimeService.cs
+++ primeservice/PrimeService.cs
@@ -2,7 +2,15 @@
 
 namespace primeservice
 {
-    public class Class1
+    public class PrimeService
     {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
     }
 }
